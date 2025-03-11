889882b572bd719f507f42c36819fe865a9fe745
Eosocram <socramlmonteiro@gmail.com>
2025-03-11 08:59:49 -0300

    feat(core): adiciona a estrutura principal do programa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Entity;
+
+namespace Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Digite o seu nome:");
+            funcionario.Nome = Console.ReadLine() ?? "";
+
+            Console.Write($"{funcionario.Nome} digite o seu CPF:");
+            string inputCPF = Console.ReadLine() ?? "";
+            if (long.TryParse(inputCPF, out long cpf))
+            {
+                funcionario.RegistroUnico = cpf;
+            }
+            else
+            {
+                Console.WriteLine("CPF inválido.");
+            }
+
+            Console.Write($"{funcionario.Nome} digite a data do seu nascimento:");
+            string inputData = Console.ReadLine() ?? "";
+            if (DateTime.TryParse(inputData, out DateTime dataNascimento))
+            {
+                funcionario.DataNascimento = dataNascimento;
+                
+            }
+            else
+            {
+                Console.WriteLine("Data de nascimento inválida.");
+            }
+
+
+            Console.Write($"{funcionario.Nome} digite o seu endereço:");
+            funcionario.Endereco = Console.ReadLine() ?? "";
+
+            Console.Write("Quando você entrou no seu emprego atual?");
+            
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime anoAdmissao))
+            {
+                funcionario.DataAdmissao = anoAdmissao;
+                Console.WriteLine($"Você entrou no emprego em: {funcionario.DataAdmissao.Year}");
+                funcionario.TempodeContribuicao();
+            }
+            else
+            {
+                Console.WriteLine("Data inválida.");
+            }
+        }
+    }
+}
